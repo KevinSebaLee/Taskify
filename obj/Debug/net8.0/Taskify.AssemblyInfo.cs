9c23804137d05225f36f65f94049b152cb1cb266
KevinSebaLee <48044115@est.ort.edu.ar>
2024-10-07 10:40:18 -0300

    Cambios dentro del header y en el index

--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828e1401f503c506bbacab57f7e2783846f4a47a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7bf22c50191bb9a251ccfbd81a2446fc097efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
