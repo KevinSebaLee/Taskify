dc106e0aa9af1539fdf91bb4809215376a016a3f
KevinSebaLee <48044115@est.ort.edu.ar>
2024-10-08 08:00:51 -0300

    Fix en Usuario.cs y agregado de fecha de nacimiento

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,9 +5,11 @@ public class Usuario{
     public string Nombre { get; set; }
     public string Apellido { get; set; }
     public string Contrasenia { get; set; }
+    public int Edad {get; set;}
     public string Email { get; set; }
-    public string NumeroTelefono { get; set; }
-    public bool Participa { get; set; }
+    public string NumeroTelefono { get; set;}
+    public int Puntaje {get; set;}
     public string FotoDePerfil { get; set; }
     public string Descripcion { get; set; }
+    public DateTime FechaNacimiento {get; set;}
 }
--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c23804137d05225f36f65f94049b152cb1cb266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c05fca5aec044f20910ba1d9e9dcb71acb0499")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
