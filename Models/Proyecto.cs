e326a4dd901bf7c1115bda70b9ddad3fde0e4ad4
AriJusid <48042387@est.ort.edu.ar>
2024-10-21 09:06:04 -0300

    Update Proyecto.cs

--- Models/Proyecto.cs
+++ Models/Proyecto.cs
@@ -2,6 +2,11 @@ public class Proyecto
 {
     public int IdProyecto { get; set; }
     public int IdCategoria { get; set; }
+    public int IdFiltro {get; set; }
+    public string Nombre {get; set;}
+    public string NombreEmpresa {get; set;}
+    public List<Rol> Roles {get; set;}
+    public string Ubicacion {get; set;}
     public DateTime FechaPublicacion { get; set; }
     public int CantIntegrantes { get; set; }
     public string Descripcion { get; set; }
--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a13c0f720ca8bdf7518e0bd18e2237e112b683")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99f1a07cc82d70c710fd2aa89d3990f91313aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
