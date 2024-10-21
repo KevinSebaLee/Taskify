a99f1a07cc82d70c710fd2aa89d3990f91313aad
AriJusid <48042387@est.ort.edu.ar>
2024-10-21 09:02:32 -0300

    Fix en que no dejaba ejecutar

--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b01dbaee97198d07ad7b74732deca2be4d1ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a13c0f720ca8bdf7518e0bd18e2237e112b683")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
