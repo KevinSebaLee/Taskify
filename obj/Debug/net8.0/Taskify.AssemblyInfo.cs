158642f8c0104faf456c92d40923720bb9848f55
AriJusid <48042387@est.ort.edu.ar>
2024-10-31 13:06:36 -0300

    Cambio footer

--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203aa86f92fcc01695d2742f7dcdaff0ccef6f73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1342e3fe2fbfca0825c22e2c394cbe6a78a55f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
