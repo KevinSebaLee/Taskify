fa39cc04c1458807dbfb0ee54aa34a3560ee11f0
KevinSebaLee <48044115@est.ort.edu.ar>
2024-11-09 12:03:42 -0300

    Intenando mostrar calendario usando API2

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,1 @@
-﻿function ObtenerFechaHoy(){
-    const formCompletar = document.getElementById('FechaHoy');
-
-    const Fecha = new Date();
-
-    let Dia = Fecha.getDay();
-    let Mes = Fecha.getMonth();
-    let Año = Fecha.getFullYear();
-
-    let FechaHoy = `${Dia}-${Mes}-${Año}`;
-
-    formCompletar.value = FechaHoy;
-}
-
-ObtenerFechaHoy();
+﻿
