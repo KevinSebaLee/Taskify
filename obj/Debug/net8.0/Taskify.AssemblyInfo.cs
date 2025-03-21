8edb739414694b6cd06fd574918985723f338053
KevinSebaLee <48044115@est.ort.edu.ar>
2025-03-21 11:50:26 -0300

    Eliminar site.js

--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4ad2f987fd2fc713d836a9d1f61e03b6eb07a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+956912f84fa613166026fa1594ff59739b8cb7b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
