ec28a58ff4448032dde3175e92d354e5735e650f
AriJusid <48042387@est.ort.edu.ar>
2025-03-28 13:08:44 -0300

    Avance estilo

--- obj/Debug/net8.0/Taskify.AssemblyInfo.cs
+++ obj/Debug/net8.0/Taskify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e46aec86ed6e51905eab16150c3cb75b76ffc7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6dc24ec622abc860d454a97a282cbe1379b29fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
